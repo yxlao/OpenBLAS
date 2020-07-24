2dbfd1cd4ce32038ffcc5ea6bcdc2e4f600be70a
Yixing Lao <yixing.lao@gmail.com>
2020-07-24 01:18:17 +0000

    add set env

---
+++ setenv.bat
@@ -0,0 +1,3 @@
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+set "LIB=%CONDA_PREFIX%\Library\lib;%LIB%"
+set "CPATH=%CONDA_PREFIX%\Library\include;%CPATH%"
